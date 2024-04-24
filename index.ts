9e4b614917a91e1dbe3d2fcac8c7c8eee04c19f0
Muxammilkhan <165667118+Muxammilkhan@users.noreply.github.com>
2024-04-24 12:56:42 -0700

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,31 @@
+import inquirer from "inquirer";
+// Asking question from user thought inquirer
+
+let answers = await inquirer.prompt([
+    {message: "Enter Frist Number", type:"number", name:"fristnumber"},
+    {message:" Enter Second Number", type:"number", name:"secondnumber"},
+    {
+        message:"Select one Operator to perform operations",
+        type:"list",
+        name:"operator",
+        choices:["Addition","Subtractions","Multiplication","Division"],
+
+    },
+]);
+// condltion statments if-else
+
+if(answers.operator === "Addition"){
+    console.log(answers.fristnumber + answers.secondnumber)
+}
+else if(answers.operator === "Subtractions"){
+    console.log(answers.fristnumber - answers.secondnumber) 
+}
+else if(answers.operator === "Multiplication"){
+    console.log(answers.fristnumber * answers.secondnumber) 
+}
+else if(answers.operator === "Division"){
+    console.log(answers.fristnumber / answers.secondnumber) 
+}
+else{
+    console.log("invalid input");
+}
